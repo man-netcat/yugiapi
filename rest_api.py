1c6fdabf1c03e31e2d03a2247a0b70ba1bdd71e2
Rick Teuthof <rmt22198@outlook.com>
2024-01-19 16:43:42 +0100

    Updated api

--- rest_api.py
+++ rest_api.py
@@ -19,26 +19,35 @@ active_db: YugiDB = omegadb
 
 class CardResource(Resource):
     def get(self):
-        cards = active_db.get_cards_by_values(request.args)
-        cards_data = [card.to_dict() for card in cards]
-        return jsonify(cards_data)
+        try:
+            cards = active_db.get_cards_by_values(request.args)
+            cards_data = [card.to_dict() for card in cards]
+            return jsonify(cards_data)
+        except Exception as e:
+            return jsonify({"error": str(e)})
 
 
 class ArchResource(Resource):
     def get(self):
-        archetypes = active_db.get_archetypes_by_values(request.args)
-        archetypes_data = [archetype.to_dict() for archetype in archetypes]
-        return jsonify(archetypes_data)
+        try:
+            archetypes = active_db.get_archetypes_by_values(request.args)
+            archetypes_data = [archetype.to_dict() for archetype in archetypes]
+            return jsonify(archetypes_data)
+        except Exception as e:
+            return jsonify({"error": str(e)})
 
 
 class SetResource(Resource):
     def get(self):
-        sets = active_db.get_sets_by_values(request.args)
-        sets_data = [set.to_dict() for set in sets]
-        return jsonify(sets_data)
+        try:
+            sets = active_db.get_sets_by_values(request.args)
+            sets_data = [set.to_dict() for set in sets]
+            return jsonify(sets_data)
+        except Exception as e:
+            return jsonify({"error": str(e)})
 
 
-class RenderResource(Resource):
+class RenderCardResource(Resource):
     def get(self, card_id):
         try:
             card = active_db.get_card_by_id(card_id)
@@ -48,17 +57,32 @@ class RenderResource(Resource):
             return jsonify({"error": str(e)})
 
 
+class MappingResource(Resource):
+    def get(self):
+        try:
+            return jsonify(
+                {
+                    "card_map": active_db.card_name_id_map,
+                    "arch_map": active_db.archetype_name_id_map,
+                    "set_map": active_db.set_name_id_map,
+                }
+            )
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+
 class ConnectionResource(Resource):
     def get(self):
-        return jsonify({"active_db_name": active_db.name})
+        try:
+            return jsonify({"active_db_name": active_db.name})
+        except Exception as e:
+            return jsonify({"error": str(e)})
 
     def post(self):
         global active_db
 
         try:
-            print(request.args)
             db_uri = request.args.get("db_uri")
-            print(db_uri)
             active_db = YugiDB(db_uri)
             return jsonify({"message": "Database connection successful"})
         except Exception as e:
@@ -68,8 +92,9 @@ class ConnectionResource(Resource):
 api.add_resource(CardResource, "/card_data")
 api.add_resource(ArchResource, "/arch_data")
 api.add_resource(SetResource, "/set_data")
-api.add_resource(RenderResource, "/render/<int:card_id>")
-api.add_resource(ConnectionResource, "/set_connection")
+api.add_resource(MappingResource, "/mappings")
+api.add_resource(RenderCardResource, "/render/<int:card_id>")
+api.add_resource(ConnectionResource, "/connection")
 
 if __name__ == "__main__":
     if args.debug:
